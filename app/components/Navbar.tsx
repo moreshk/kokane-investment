a9e6552aed9d0a67426ba7c7150819c192f5eef2
moreshk <moreshk@gmail.com>
2024-07-24 09:05:32 +0530

    menu bar responsive

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,13 +1,23 @@
+'use client'
+import { useState } from 'react';
 import Link from 'next/link';
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className="bg-gray-800 p-4">
       <div className="container mx-auto flex justify-between items-center">
         <Link href="/" className="text-white text-xl font-bold">
           Kokane Investment
         </Link>
-        <ul className="flex space-x-4">
+        
+        {/* Desktop menu */}
+        <ul className="hidden md:flex space-x-4">
           <li>
             <Link href="/" className="text-white hover:text-gray-300">
               Home
@@ -24,7 +34,40 @@ const Navbar = () => {
             </Link>
           </li>
         </ul>
+
+        {/* Mobile menu button */}
+        <button 
+          className="md:hidden text-white"
+          onClick={toggleMenu}
+        >
+          <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+          </svg>
+        </button>
       </div>
+
+      {/* Mobile menu */}
+      {isMenuOpen && (
+        <div className="md:hidden">
+          <ul className="flex flex-col space-y-2 mt-4">
+            <li>
+              <Link href="/" className="block text-white hover:text-gray-300" onClick={toggleMenu}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link href="/why-should-i-invest-in-indian-stock-market" className="block text-white hover:text-gray-300" onClick={toggleMenu}>
+                Why Invest in Indian Stock Market
+              </Link>
+            </li>
+            <li>
+              <Link href="/why-should-i-invest-in-indian-stock-market-marathi" className="block text-white hover:text-gray-300" onClick={toggleMenu}>
+                भारतीय शेअर बाजारात का गुंतवणूक करावी
+              </Link>
+            </li>
+          </ul>
+        </div>
+      )}
     </nav>
   );
 };
